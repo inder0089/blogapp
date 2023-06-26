238d8f98d804b29b00d5a6acfcfaa61b1553ba59
inderjeet singh <isingh@texala.net>
2023-06-26 21:45:47 +0530

    api

--- src/api/index.js
+++ src/api/index.js
@@ -6,7 +6,10 @@ const auth = JSON.parse(window.localStorage.getItem("auth"));
 
 const privateAPI = axios.create({
   baseURL: process.env.REACT_APP_BASE_API_URL,
-  headers: { Authorization: auth?.access_token },
+  headers: {
+    Authorization: auth?.access_token,
+    "x-access-token": "fghjkfjkgiuiutgkb",
+  },
 });
 
 export default privateAPI;
--- src/pages/todo/TodoApi.js
+++ src/pages/todo/TodoApi.js
@@ -6,24 +6,39 @@ import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import Table from "react-bootstrap/Table";
 import privateAPI from "../../api";
+import axios from "axios";
+
+const auth = JSON.parse(window.localStorage.getItem("auth"));
 
 export const TodoApi = () => {
   const [inputText, setInputText] = useState("");
   const [todoList, settodoList] = useState([]);
   async function getTodos() {
-    const response = await privateAPI({
-      url: "todos",
-      headers: {
-        method: "GET",
-      },
-    });
+    // const response = await privateAPI({
+    //   url: "todos",
+    //   headers: {
+    //     method: "GET",
+    //   },
+    // });
+
+    const response = await privateAPI.get("/todos");
+
+    // const response = await axios.get(
+    //   "https://todolist-api-mz6f.onrender.com/todos",
+    //   {
+    //     headers: {
+    //       Authorization: auth?.access_token,
+    //     },
+    //   }
+    // );
+
     console.log("todo data", response);
     settodoList(response.data.todos);
     return response;
   }
 
   useEffect(() => {
-    getTodos();
+    getTodos(); //fetch todo data with get on page mount
   }, []);
 
   const initialvalue = {
@@ -38,10 +53,8 @@ export const TodoApi = () => {
 
   const formData = (e) => {
     e.preventDefault();
-    // alert("inder");
-    // settodoList([...todoList, inputText]);
     privateAPI
-      .post("/todos", inputText)
+      .post("/todos", inputText) // add API
       .then(async (response) => {
         console.log("response", response);
         await getTodos();
@@ -53,9 +66,13 @@ export const TodoApi = () => {
   };
   // console.log(todoList);
 
-  const handleCheckbox = (e, iD) => {
-    const checklist = e.target.checked;
-    console.log("handleCheckbox", checklist, iD);
+  const handleCheckbox = async (e, iD) => {
+    // const checklist = e.target.checked;
+    const checklistID = iD;
+
+    // console.log("handleCheckbox", checklist, iD);
+    await privateAPI.get(`/isCompleted/${checklistID}`);
+    await getTodos();
 
     // const checkedItem = [...todoList];
 
@@ -65,43 +82,49 @@ export const TodoApi = () => {
     // console.log("filtedList", filtedList);
   };
 
+  const deleteItem = async (iD) => {
+    const deleteItemID = iD;
+    // console.log("id", deleteItemID);
+    await privateAPI.delete(`/todos/${deleteItemID}`);
+    await getTodos();
+  };
+
   return (
     <div>
-      <div className="todo">
-        <div className="container">
+      <div className='todo'>
+        <div className='container'>
           <h1>Todo</h1>
-          <div className="row">
-            <div className="col-md-6">
+          <div className='row'>
+            <div className='col-md-6'>
               <form onSubmit={formData}>
-                <InputGroup className="mb-3">
+                <InputGroup className='mb-3'>
                   <Form.Control
-                    placeholder="Enter Title"
+                    placeholder='Enter Title'
                     aria-label="Recipient's username"
-                    aria-describedby="basic-addon2"
-                    name="title"
+                    aria-describedby='basic-addon2'
+                    name='title'
                     value={inputText.title}
                     onChange={handlechange}
                   />
                   <button
-                    type="submit"
-                    variant="outline-secondary"
-                    id="button-addon2"
-                  >
+                    type='submit'
+                    variant='outline-secondary'
+                    id='button-addon2'>
                     Add
                   </button>
                 </InputGroup>
               </form>
             </div>
-            <div className="col-md-6">
-              <select name="filter" id="select-list">
-                <option value="all">All</option>
-                <option value="completed">Completed</option>
-                <option value="notCompleted">Not completed</option>
+            <div className='col-md-6'>
+              <select name='filter' id='select-list'>
+                <option value='all'>All</option>
+                <option value='completed'>Completed</option>
+                <option value='notCompleted'>Not completed</option>
               </select>
             </div>
           </div>
-          <div className="todo-table">
-            <Table striped bordered hover variant="dark">
+          <div className='todo-table'>
+            <Table striped bordered hover variant='dark'>
               <thead>
                 <tr>
                   <th>#</th>
@@ -115,18 +138,22 @@ export const TodoApi = () => {
                     return (
                       <tr key={list._id}>
                         <td>
-                          <div className="mb-3">
+                          <div className='mb-3'>
                             <input
-                              type="checkbox"
-                              name="group1"
+                              type='checkbox'
+                              name='group1'
                               checked={list?.isCompleted}
                               onClick={(e) => handleCheckbox(e, list._id)}
                             />
                           </div>
                         </td>
-                        <td className="w-75">{list.title}</td>
+                        <td className='w-75'>{list.title}</td>
                         <td>
-                          <Button variant="danger">Delete</Button>
+                          <Button
+                            variant='danger'
+                            onClick={() => deleteItem(list._id)}>
+                            Delete
+                          </Button>
                         </td>
                       </tr>
                     );
